84d06298f41c26a72c7c68dffcd95fd465dc2540
horizonzy <1060026287@qq.com>
2020-12-04 13:03:35 +0800

    use newStub to replace newBlockingStub

--- src/main/java/com/zy/GrpcClient.java
+++ src/main/java/com/zy/GrpcClient.java
@@ -3,17 +3,38 @@ package com.zy;
 import com.example.grpc.gencode.HelloRequest;
 import com.example.grpc.gencode.HelloResponse;
 import com.example.grpc.gencode.HelloServiceGrpc;
+import com.example.grpc.gencode.HelloServiceGrpc.HelloServiceStub;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
 
 public class GrpcClient {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ManagedChannel channel = null;
-        channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext(true).build();
-        HelloServiceGrpc.HelloServiceBlockingStub stub = HelloServiceGrpc.newBlockingStub(channel);
-        HelloResponse response = stub.hello(HelloRequest.newBuilder().setFirstName("horizon").setLastName("zhao").build());
-        System.out.println(response.getGreeting());
+        channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
+        HelloServiceStub stub = HelloServiceGrpc.newStub(channel);
+
+        stub.hello(HelloRequest.newBuilder().setFirstName("horizon").setLastName("zhao").build(), new StreamObserver<HelloResponse>() {
+            @Override
+            public void onNext(HelloResponse value) {
+                System.out.println(value.getGreeting());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+
+            }
+        });
+
+        System.in.read();
     }
 
 }
